17563741d0f5085bc7c39a43e074f5a8eb9a9067
Deekshitha <deekshithachatla@gmail.com>
2023-10-27 19:53:08 +0530

    Add project files.

---
+++ Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practiceproject_OOP
+{
+    public class Teacher
+    {
+
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+        public Teacher()
+        {
+
+        }
+    }
+}
